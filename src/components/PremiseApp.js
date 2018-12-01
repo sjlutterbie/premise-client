f61b6d0780ceff5b165f820b980de64099a43ef4
sjlutterbie <simon.lutterbie@ltrbe.com>
2018-12-01 18:35:25 +0000

    Created Header component

--- src/components/PremiseApp.js
+++ src/components/PremiseApp.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import Header from './Header';
+
 export default function PremiseApp(props) {
+
   return (
     <div>
+      <Header />
       <p>Hello, world!</p>
     </div>
   );
