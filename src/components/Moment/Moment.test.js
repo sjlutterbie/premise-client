07b05a4d8d257bfd8b46da8efc9990d2ae3bbe7d
sjlutterbie <simon.lutterbie@ltrbe.com>
2018-12-07 15:53:23 +0000

    Basic <Moment/> structure created

--- src/components/Moment/Moment.test.js
+++ src/components/Moment/Moment.test.js
@@ -16,22 +16,75 @@ const testProps = {
   moment: testMoment
 };
 
+// Create object to hold shallow render
+let wrapper;
 
 describe('<Moment />', () => {
+  
+  // Build the <Moment/> component before each test
+  beforeEach(() => {
+    wrapper = shallow(<Moment {...testProps}/>);
+  });
   
   it('Renders without crashing', () => {
     shallow(<Moment {...testProps}/>);
   });
   
   it('Has three primary components', () => {
-    const wrapper = shallow(<Moment {...testProps}/>);
     expect(wrapper.children().length).toEqual(3);
   });
   
-  it('Renders momentText in the correct place', () => {
-    const wrapper = shallow(<Moment {...testProps}/>);
-    const para = wrapper.find('.moment-text');
-    expect(para.text()).toEqual(testProps.moment.text);
+  describe('.moment-text area', () => {
+    it('Renders momentText in the correct place', () => {
+      const textArea = wrapper.find('.moment-text');
+      expect(textArea.text()).toEqual(testProps.moment.text);
+    });    
   });
+  
+  describe('.action-buttons area', () => {
+    
+    it('Has two buttons', () => {
+      const actionArea = wrapper.find('.action-buttons');
+      expect(actionArea.find('button').length).toEqual(2);
+    });
+    
+    it('Has correctly named buttons', () => {
+      const actionArea = wrapper.find('.action-buttons');
+      const buttons = actionArea.find('button');
+      expect(buttons.find('.open-create-mode').length).toEqual(1);
+      expect(buttons.find('.switch-story-branch').length).toEqual(1);
+    });
+    
+  });
+  
+  describe('Create moment form', () => {
+    
+    let formArea;
+    
+    beforeEach(() => {
+      formArea = wrapper.find('form');
+    });
+    
+    it('Exists', () => {
+      expect(wrapper.find('form').length).toEqual(1);
+    });
+    
+    it('Has a single textarea element', () => {
+      expect(formArea.find('textarea').length).toEqual(1);
+    });
+    
+    it('Has two buttons', () => {
+      expect(formArea.find('button').length).toEqual(2);
+    });
+    
+    it('Has correctly labeled buttons', () => {
+      const buttons = formArea.find('button');
+      expect(buttons.find('.cancel-create-moment').length).toEqual(1);
+      expect(buttons.find('.create-moment').length).toEqual(1);
+    });
+
+  });
+
+
   
 });
--- src/components/Moment/index.js
+++ src/components/Moment/index.js
@@ -14,8 +14,18 @@ export class Moment extends Component{
     return (
       <div className="moment">
         <p className="moment-text">{this.props.moment.text}</p>
-        <div className="action-buttons"></div>
-        <div className="create-moment-form"></div>
+        <div className="action-buttons">
+          <button className="open-create-mode">Create</button>
+          <button className="switch-story-branch">Switch branch</button>
+        </div>
+        <form className="create-moment-form">
+          <fieldset>
+            <legend>Add to the story</legend>
+            <textarea></textarea>
+            <button className="cancel-create-moment">Cancel</button>
+            <button className="create-moment">Create</button>
+          </fieldset>
+        </form>
       </div>
     );
   }  
