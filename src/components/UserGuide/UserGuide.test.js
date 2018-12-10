61af307ef6a8e934467502305a7b2bbc785a782a
sjlutterbie <simon.lutterbie@ltrbe.com>
2018-12-10 21:05:14 +0000

    <UserGuide/> 'Return to app' button added

--- src/components/UserGuide/UserGuide.test.js
+++ src/components/UserGuide/UserGuide.test.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 
-import UserGuide from './index';
+import {UserGuide} from './index';
+
+
+let props = {
+  responsiveBracket: 'small',
+  addVisiblePanes: jest.fn(),
+  removeVisiblePanes: jest.fn()
+};
+let wrapper;
 
 describe('<UserGuide />', () => {
+  
+  beforeEach(() => {
+    wrapper = shallow(<UserGuide {...props}/>);
+  });
   
   it('Renders without crashing', () => {
     shallow(<UserGuide />);
   });
   
+  it('Has the correct structure', () => {
+    expect(wrapper.find('button.rct-exit-userguide').length).toEqual(1);
+    expect(wrapper.find('.userguide-content-area').length).toEqual(1);
+  });
+  
+  describe('Event handling', () => {
+    
+    it('Clicking .rct-exit-userguide calls actions as expected', () => {
+      const testCases = [
+        // [responsiveBracket, expectedAddPanes, expectedRemovePanes]
+        ['small', ['reader'], ['userguide']],
+        ['large', [], ['userguide']]
+      ];
+      
+      testCases.forEach(testCase => {
+        props.responsiveBracket = testCase[0];
+        wrapper = shallow(<UserGuide {...props}/>);
+        wrapper.find('.rct-exit-userguide').simulate('click');
+        expect(props.addVisiblePanes)
+          .toHaveBeenCalledWith(testCase[1]);
+        expect(props.removeVisiblePanes)
+          .toHaveBeenCalledWith(testCase[2]);
+      });
+    });
+  });
 });
--- src/components/UserGuide/index.js
+++ src/components/UserGuide/index.js
@@ -1,16 +1,49 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
 import './UserGuide.css';
+
+import {addVisiblePanes, removeVisiblePanes} from '../../actions';
 
 // <UserGuide /> is a 'routable' area of the site, that includes the
 //  introductory slideshow and, eventually, other tools, such as an FAQ.
 
-export default function UserGuide(props) {
+export function UserGuide(props) {
+  // Expected props:
+  //  State:
+  //    responsiveBracket: string
+  //  Actions:
+  //    addVisiblePanes(array)
+  //    removeVisiblePanes(array)
   
   return (
     <div className="user-guide">
-      <p>User Guide Area</p>
+      <button className="rct-exit-userguide"
+              onClick={() => {
+                // Handle based on responsive bracket
+                let removePanes = ['userguide'];
+                let addPanes = [];
+                if(props.responsiveBracket === 'small') {
+                  addPanes = ['reader'];
+                }
+                props.addVisiblePanes(addPanes);
+                props.removeVisiblePanes(removePanes);
+              }}>Return to App</button>
+      <div className="userguide-content-area">
+        <p>User Guide Area</p>
+      </div>
     </div>
   );
   
 }
 
+export const mapStateToProps = state => ({
+  responsiveBracket: state.navState.responsiveBracket
+});
+
+export const mapDispatchToProps = {
+  addVisiblePanes,
+  removeVisiblePanes
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserGuide);
