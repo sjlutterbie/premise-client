60665a66996abbc4bd3ac2b21943f76e505846c0
sjlutterbie <simon.lutterbie@ltrbe.com>
2019-01-08 19:56:30 +0000

    Created placeholder <RegistrationForm />

---
+++ src/components/RegistrationForm/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+import './RegistrationForm.css';
+
+export function RegistrationForm(props) {
+  
+  return (
+    <div className="registration-form">
+      This is the RegistrationForm!
+    </div>
+  );
+}
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = state => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(RegistrationForm);
