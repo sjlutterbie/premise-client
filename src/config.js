e5c417e7b7750c2b173c58eb2d98787d8d24bc03
sjlutterbie <simon.lutterbie@ltrbe.com>
2019-01-17 00:12:01 +0000

    Updated config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,2 @@
 exports.REACT_APP_PREMISE_BASE_API_URL = process.env.REACT_APP_PREMISE_BASE_API_URL ||
-//  'https://rocky-chamber-71700.herokuapp.com/api';
-  'http://52.24.55.126:8080/api';
+  'https://rocky-chamber-71700.herokuapp.com/api';
